1c8f9530b57830b37e118d7785b2c8bbb7834419
Trent Mick <trent.mick@elastic.co>
2024-02-02 01:38:30 +0000

    update locust usage to load both node-server instances

--- Elastiflix/locustfile.py
+++ Elastiflix/locustfile.py
@@ -1,12 +1,29 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 from time import sleep
 import random
 
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
+class NodeServerElasticApm(HttpUser):
+    host = "http://node-server-elastic-apm:3001"
+    wait_time = between(1, 3)
 
+    @task
+    def favorites_and_login(self):
+        self.client.get("/api/favorites")
+
+        if random.random() < 0.5:
+            self.client.post("/api/favorites", json={"id": random.randint(1, 100)})
+
+        # in 50% of the cases, call the login endpoint
+        if random.random() < 0.5:
+            self.client.get("/api/login")
+
+class NodeServerElasticOtel(HttpUser):
+    host = "http://node-server-elastic-otel:3002"
+    wait_time = between(1, 3)
+
+    @task
+    def favorites_and_login(self):
         self.client.get("/api/favorites")
 
         if random.random() < 0.5:
@@ -15,4 +32,4 @@ class HelloWorldUser(HttpUser):
         # in 50% of the cases, call the login endpoint
         if random.random() < 0.5:
             self.client.get("/api/login")
-        sleep(1)
+
