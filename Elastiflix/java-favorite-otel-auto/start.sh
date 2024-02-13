c2789946e0b3e300f9f05b60227d9067fa2f6ba4
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-02-13 11:55:59 +0100

    add distribution config for java

--- Elastiflix/java-favorite-otel-auto/start.sh
+++ Elastiflix/java-favorite-otel-auto/start.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
-java -javaagent:/otel/opentelemetry-javaagent.jar \
+
+agent_path=''
+case "${OTEL_DISTRIBUTION:-otel}" in
+  elastic)
+    agent_path='/elastic/elastic-otel-javaagent.jar'
+    ;;
+  otel)
+    agent_path='/otel/opentelemetry-javaagent.jar'
+    ;;
+  *)
+    echo "unknown otel distribution: ${OTEL_DISTRIBUTION}"
+    exit 1
+  ;;
+esac
+
+java -javaagent:${agent_path} \
 -jar /usr/src/app/target/favorite-0.0.1-SNAPSHOT.jar --server.port=5000
