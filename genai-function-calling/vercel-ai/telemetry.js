79e006c94e261c895488b65c5200e16f4e1b51b3
Adrian Cole <adrian.cole@elastic.co>
2025-04-25 11:46:42 +0800

    all good
    Signed-off-by: Adrian Cole <adrian.cole@elastic.co>

--- genai-function-calling/vercel-ai/telemetry.js
+++ genai-function-calling/vercel-ai/telemetry.js
@@ -1,9 +1,8 @@
-// Import and initialize MCPInstrumentation before importing the MCP modules.
 const { MCPInstrumentation } = require("@arizeai/openinference-instrumentation-mcp");
-const mcpInstrumentation = new MCPInstrumentation();
 
-// Then, manually instrument MCP, as it doesn't have a traditional module structure.
+const mcpInstrumentation = new MCPInstrumentation();
+// MCP must be manually instrumented as it doesn't have a traditional module structure
 mcpInstrumentation.manuallyInstrument({
-    clientModule: require("@modelcontextprotocol/sdk/client/index.js"),
-    serverModule: require("@modelcontextprotocol/sdk/server/index.js"),
+    clientStdioModule: require("@modelcontextprotocol/sdk/client/stdio.js"),
+    serverStdioModule: require("@modelcontextprotocol/sdk/server/stdio.js"),
 });
